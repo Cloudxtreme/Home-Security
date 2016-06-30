b9675319647dcb4e987ddf47fa75b31b2a46fcb8
Gabe Harms <gabeharms@gmail.com>
2016-06-30 10:45:10 -0700

    Introducing Authentication into the Websocket Connection

--- Web-API/server/components/authentication/auth.js
+++ Web-API/server/components/authentication/auth.js
@@ -8,11 +8,30 @@ export function isAuthenticated(req,res,next) {
     var token = req.headers['x-auth'];
     var auth = jwt.decode(token, secretKey);
     User.findOneAsync({username: auth.username})
-    .then(function(user) {
-      req.user = user;
-      next();
-    });
+      .then(function(user) {
+        req.user = user;
+        next();
+      });
   } catch(err) {
     return res.sendStatus(401);
   }
+}
+
+export function isTokenValid(token) {
+  return new Promise(function(resolve, reject) {
+    try {
+      var auth = jwt.decode(token, secretKey);
+      return User.findOneAsync({username: auth.username})
+        .then(function(user) {
+          if (user) { 
+            return resolve(user)
+          }
+          else {
+            return reject('Not Authenticated');
+          }
+        });
+    } catch(err) {
+      return reject('Not Authenticated');
+    }
+  });
 }
